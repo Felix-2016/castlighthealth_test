ba913e8cbf7ba1d111c6d47e80d0243493281b86
Felix-2016 <felix2016@gmail.com>
2018-06-19 15:54:06 +0530

    Added a testcase.

--- lib/ui/login/login_page.py
+++ lib/ui/login/login_page.py
@@ -16,5 +16,60 @@ class LoginPage():
         except:
             return None
 
+    def get_signin_button(self):
+        try:
+            return self.browser.find_element_by_xpath("//button[@type='submit']")
+        except:
+            return None
+
+    def get_register_link(self):
+        try:
+            return self.browser.find_element_by_xpath("(//a[text()='Register'])[2]")
+        except:
+            return None
+
+    def get_username1_textbox(self):
+        try:
+            return self.browser.find_element_by_id("email")
+        except:
+            return None
+
+    def get_password1_textbox(self):
+        try:
+            return self.browser.find_element_by_id("password")
+        except:
+            return None
+
+    def get_login_error_msg(self):
+        try:
+            return self.browser.find_element_by_xpath("//div[contains(text(),'The email or password')]")
+        except:
+            return None
+
+    def get_forgot_password_link(self):
+        try:
+            return self.browser.find_element_by_xpath("//a[text()='Forgot Password?']")
+        except:
+            return None
+
+    def get_email_reset_textbox(self):
+        try:
+            return self.browser.find_element_by_id('email-address')
+        except:
+            return None
+
+    def get_submit_button(self):
+        try:
+            return self.browser.find_element_by_xpath("//button[@type='submit']")
+        except:
+            return None
+
+    def get_password_reset_error(self):
+        try:
+            return self.browser.find_element_by_xpath("//div[contains(text(),'The email address')]")
+        except:
+            return None
+
+
 
 
